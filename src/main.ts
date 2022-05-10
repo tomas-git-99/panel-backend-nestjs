fcce4f35e9df871459d430f991077b3b455c32e4
Tomas Lopez <tomasgit@MacBook-Air-de-Tomas.local>
2022-05-10 02:53:09 -0300

    final

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(9000);
   //await app.listen(3000, '0.0.0.0');
 }
 
